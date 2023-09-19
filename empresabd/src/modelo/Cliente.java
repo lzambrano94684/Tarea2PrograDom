e1dc7d1c776f16abaa6473a3de80d14c268bdc72
lzambrano94684 <87407268+lzambrano94684@users.noreply.github.com>
2023-09-18 22:21:27 -0600

    Add files via upload

---
+++ empresabd/src/modelo/Cliente.java
@@ -0,0 +1,149 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.swing.table.DefaultTableModel;
+
+/**
+ *
+ * @author Luis Zambra
+ */
+public class Cliente extends Persona {
+    
+    
+    private String nit;
+    
+    private int id;
+    Conexion cn;
+    
+public Cliente() {}
+
+    public Cliente(String nit, int id, String nombres, String apellidos, String direccion, String telefono, String fecha_nacimiento) {
+        super(nombres, apellidos, direccion, telefono, fecha_nacimiento);
+        this.nit = nit;
+        this.id = id;
+    }
+
+    
+    
+    public String getNit() {
+        return nit;
+    }
+
+    public void setNit(String nit) {
+        this.nit = nit;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    public DefaultTableModel leer(){
+        DefaultTableModel tabla = new DefaultTableModel();
+        try{
+            cn = new Conexion();
+            cn.abrir_conexion();
+            String query;
+            query = "select id_clientes as id,nit,nombres,apellidos,direccion,telefono,fecha_nacimiento from clientes;";
+            ResultSet consulta = cn.conexionBD.createStatement().executeQuery(query);
+            String encabezado[] = {"id", "Nit", "Nombres", "Apellidos", "Direccion", "Telefono", "Nacimiento"};
+            tabla.setColumnIdentifiers(encabezado);
+            String datos[] = new String[7];
+            while (consulta.next()){
+                datos[0] = consulta.getString("id");
+                datos[1] = consulta.getString("nit");
+                datos[2] = consulta.getString("nombres");
+                datos[3] = consulta.getString("apellidos");
+                datos[4] = consulta.getString("direccion");
+                datos[5] = consulta.getString("telefono");
+                datos[6] = consulta.getString("fecha_nacimiento");
+                tabla.addRow(datos);
+            }
+            
+            cn.cerrar_conexion();
+        }catch(SQLException ex){
+            cn.cerrar_conexion();
+            System.out.println("Error: "+ ex.getMessage() );
+        }
+        
+            
+        
+        return tabla;
+        
+    }
+    
+    public void crear(){
+        
+        try{
+            PreparedStatement parametro;
+            cn = new Conexion();
+            cn.abrir_conexion();
+            String query;
+            query = "insert into clientes (nit,nombres,apellidos,direccion,telefono,fecha_nacimiento) values (?,?,?,?,?,?);";
+            parametro = (PreparedStatement) cn.conexionBD.prepareStatement(query);
+            parametro.setString(1,getNit());
+            parametro.setString(2,getNombres());
+            parametro.setString(3,getApellidos());
+            parametro.setString(4,getDireccion());
+            parametro.setString(5,getTelefono());
+            parametro.setString(6,this.getFecha_nacimiento());
+           int executar = parametro.executeUpdate();
+            System.out.println("Se inserto: " + Integer.toString(executar) + "Registro");
+            cn.cerrar_conexion();
+        }catch ( Exception ex){
+            System.out.println("Error:" + ex.getMessage());
+        }
+    }
+    
+    public void actualizar(){
+        
+        try{
+            PreparedStatement parametro;
+            cn = new Conexion();
+            cn.abrir_conexion();
+            String query;
+            query = "update clientes set nit=?,nombres=?,apellidos=?,direccion=?,telefono=?,fecha_nacimiento=? where id_clientes=?;";
+            parametro = (PreparedStatement) cn.conexionBD.prepareStatement(query);
+            parametro.setString(1,getNit());
+            parametro.setString(2,getNombres());
+            parametro.setString(3,getApellidos());
+            parametro.setString(4,getDireccion());
+            parametro.setString(5,getTelefono());
+            parametro.setString(6,this.getFecha_nacimiento());
+            parametro.setInt(7, getId());
+           int executar = parametro.executeUpdate();
+            System.out.println("Se actualizo: " + Integer.toString(executar) + "Registro");
+            cn.cerrar_conexion();
+        }catch ( Exception ex){
+            System.out.println("Error:" + ex.getMessage());
+        }
+    }
+    
+    public void borrar(){
+        try{
+            PreparedStatement parametro;
+            cn = new Conexion();
+            cn.abrir_conexion();
+            String query;
+            query = "delete from clientes where id_clientes =?;";
+            parametro = (PreparedStatement) cn.conexionBD.prepareStatement(query);
+            parametro.setInt(1, getId());
+           int executar = parametro.executeUpdate();
+            System.out.println("Se elimino: " + Integer.toString(executar) + "Registro");
+            cn.cerrar_conexion();
+        }catch ( Exception ex){
+            System.out.println("Error:" + ex.getMessage());
+        }
+        
+        
+    }
+    
+}
---
+++ empresabd/src/vista/main.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package vista;
+import modelo.Conexion;
+/**
+ *
+ * @author Luis Zambra
+ */
+public class main {
+    
+    
+    
+    
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+       /*Conexion cn = new Conexion ();
+       cn.abrir_conexion();
+       cn.cerrar_conexion();*/
+       
+       frm_cliente frm = new frm_cliente();
+                frm.show();
+       
+    }
+    
+}
